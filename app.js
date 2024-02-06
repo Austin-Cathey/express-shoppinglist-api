a5b22b9fb1eb865d6a74c4203cabc3365b78577d
Austin-Cathey <austincatheyart@gmail.com>
2024-02-06 12:48:13 -0500

    need testing, but have base code

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+
 const app = express();
+
 //mongoose stuff
 const mongoose = require('mongoose');
 const ShoppingList = require("./models/ShoppingList");
@@ -8,18 +13,20 @@ mongoose.connect(db_url)
 const db = mongoose.connection
 db.once("open", () => console.log("connected to mongoDB"))
 
+//endpoints
+//GET all
 app.get('/shoppinglists', (req, res) => {
     ShoppingList.find().then((results) => res.status(200).json(results))
 })
-
+//POST new list
 app.post('/shoppinglists', (req, res) => {
     const newShoppingList = new ShoppingList(req.body)
     newShoppingList.save()
     res.status(201).json(newShoppingList)
 })
-
+//GET target list
 app.get('/shoppinglists/:shoppinglistId', (req, res) => {
-    ShoppingList.findById(req.params.ShoppingListId)
+    ShoppingList.findById(req.params.shoppinglistId)
       .then((results) => {
         if (results) {
           res.status(200).json(results)
@@ -30,5 +37,11 @@ app.get('/shoppinglists/:shoppinglistId', (req, res) => {
       .catch((error) => res.status(400).json({ message: 'Bad request' }))
   })
   
+  //all-else error
+  app.get('*', function (req, res) {
+    res.status(404).json({ error: 'route not found' })
+  })
+
+  //PORT
 const config = { port: process.env.PORT || 3000 };
 app.use(express.json)
--- models/ShoppingList.js
+++ models/ShoppingList.js
@@ -1,5 +1,8 @@
 
 const mongoose = require('mongoose')
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
 
 const shoppingListSchema = new mongoose.Schema({
     title: {type: String, max: 50, required: true},
